0fa9dffd02b943c07ee5436e2177906a696f587c
Ashuu02 <ashushimpi02@gmail.com>
2022-08-28 20:25:13 +0530

    added project and projectItems

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import netflix from "../public/assets/Project/netflix-clone.png";
+import Link from "next/link";
+import Projectitem from "./Projectitem";
+import krypt from "../public/assets/Project/krypt.png"
+import expenseTracker from "../public/assets/Project/expense-tracker.png"
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div id="projects" className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="text-xl tracking-widest uppercase text-[#5651e5]">
+          Projects
+        </p>
+        <h2 className="py-4">What I've Built</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+          <Projectitem
+            title={"Netflix-Clone"}
+            backgroundImg={netflix}
+            projectUrl="/netflixClone"
+          />
+          <Projectitem
+            title={"Crypto Wallet"}
+            backgroundImg={krypt}
+            projectUrl="/krypt"
+          />
+          <Projectitem
+            title={"Expense-tracker"}
+            backgroundImg={expenseTracker}
+            projectUrl="/expenseTracker"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
